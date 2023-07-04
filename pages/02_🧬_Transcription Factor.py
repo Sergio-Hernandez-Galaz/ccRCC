72fa4e5b074bdbca6c1a1f9156740b1ec7d49a97
Sergio <111708707+Sergio-Hernandez-Galaz@users.noreply.github.com>
2023-07-04 11:18:15 -0400

    update

--- pages/02_🧬_Transcription Factor.py
+++ pages/02_🧬_Transcription Factor.py
@@ -6,6 +6,12 @@ import io
 import base64
 import pickle 
 from PIL import Image
+import pandas as pd
+
+############################################################################################################
+df = pd.read_csv("todos.csv")
+
+
 
 #############################################################################################################
 with open('regulons.pkl', 'rb') as f:
@@ -24,16 +30,23 @@ def upload_sc(adata):
     # IMPORTANT: Cache the conversion to prevent computation on every rerun
     return sc.read_h5ad(adata)
 ##############################################################################################################
-
+st.dataframe(df)
+st.divider()
 top = st.sidebar.multiselect("Top Regulons",regulons_list)
+encoded_image_list = []
+img_list = []
 if top:
     for i in top:
          with io.open(f"regulon_figs/Regulon({i}(+)).png", 'rb') as archivo:
             imagen_bytes = archivo.read()
             encoded_image = base64.b64encode(imagen_bytes).decode('utf-8')
             imagen = Image.open(io.BytesIO(imagen_bytes))
-            st.image(imagen,width=670)
-            st.markdown(image_to_button(encoded_image,f"regulon_{i}.png"),unsafe_allow_html=True)
+            encoded_image_list.append(encoded_image)
+            img_list.append(imagen) 
+            # st.image(imagen,width=420)
+            # st.markdown(image_to_button(encoded_image,f"regulon_{i}.png"),unsafe_allow_html=True)
+st.image(img_list,width=420,caption=st.markdown(image_to_button(encoded_image,f"regulon_{i}.png"),unsafe_allow_html=True))
+# st.markdown(image_to_button(encoded_image,f"regulon_{i}.png"),unsafe_allow_html=True)
     
    
 
