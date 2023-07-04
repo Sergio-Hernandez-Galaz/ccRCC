357d3ff724eabbe857385a04207db44ff8a14250
Sergio <111708707+Sergio-Hernandez-Galaz@users.noreply.github.com>
2023-07-04 01:18:15 -0400

    uff

--- pages/02_🧬_Transcription Factor.py
+++ pages/02_🧬_Transcription Factor.py
@@ -12,8 +12,7 @@ with open('regulons.pkl', 'rb') as f:
     regulons_list = pickle.load(f)
 ##############################################################################################################
 def image_to_button(fig,name):
-            b64 = base64.b64encode(fig.getvalue()).decode()
-            htm = f'<a href="data:file/txt;base64,{b64}" download="{name}"><input type="button" value="Download Image"></a>'
+            htm = f'<a href="data:file/txt;base64,{fig}" download="{name}"><input type="button" value="Download Image"></a>'
             return htm
 
 st.set_page_config(layout="wide")
@@ -34,8 +33,9 @@ if top:
     for i in top:
          with io.open(f"regulon_figs/Regulon({i}(+)).png", 'rb') as archivo:
             imagen_bytes = archivo.read()
+            encoded_image = base64.b64encode(imagen_bytes).decode('utf-8')
             imagen = Image.open(io.BytesIO(imagen_bytes))
-            st.markdown(image_to_button(imagen,"regulon_{i}.png"),unsafe_allow_html=True)
+            st.markdown(image_to_button(encoded_image,"regulon_{i}.png"),unsafe_allow_html=True)
             img_list.append(imagen)
             
     st.image(img_list,width=435)
