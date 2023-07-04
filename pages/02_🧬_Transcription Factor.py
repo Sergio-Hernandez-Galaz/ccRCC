6b9e120040da92fb1778ef9b1e6f831a9b958e1a
Sergio <111708707+Sergio-Hernandez-Galaz@users.noreply.github.com>
2023-07-04 00:59:00 -0400

    juesu

---
+++ pages/02_🧬_Transcription Factor.py
@@ -0,0 +1,47 @@
+# Contents of ~/my_app/pages/page_3.py
+import streamlit as st
+import scanpy as sc
+import matplotlib.pyplot as plt
+import io
+import base64
+import pickle 
+from PIL import Image
+
+#############################################################################################################
+with open('regulons.pkl', 'rb') as f:
+    regulons_list = pickle.load(f)
+##############################################################################################################
+def image_to_button(image,name):
+            img = io.BytesIO()
+            image.savefig(img, format='png',dpi=300,bbox_inches='tight')
+            img.seek(0)
+            b64 = base64.b64encode(img.read()).decode()
+            htm = f'<a href="data:file/txt;base64,{b64}" download="{name}"><input type="button" value="Download Image"></a>'
+            return htm
+
+st.set_page_config(layout="wide")
+st.set_option('deprecation.showPyplotGlobalUse', False)
+sc.set_figure_params(dpi=300)
+
+@st.cache_resource
+def upload_sc(adata):
+    # IMPORTANT: Cache the conversion to prevent computation on every rerun
+    return sc.read_h5ad(adata)
+##############################################################################################################
+st.sidebar.write('## scRNA-vis Beta')
+st.sidebar.write("Webapp for visualization of ccRCC single cell transcripts. Created by **Sergio Hernández** 🧬")
+
+top = st.sidebar.multiselect("Top Regulons",regulons_list)
+img_list = []
+if top:
+    for i in top:
+         with io.open(f"regulon_figs/Regulon({i}(+)).png", 'rb') as archivo:
+            imagen_bytes = archivo.read()
+            imagen = Image.open(io.BytesIO(imagen_bytes))
+            img_list.append(imagen)
+    st.image(img_list,width=435)
+
+     
+
+
+
