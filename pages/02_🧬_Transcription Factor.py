7fdaf8a8642bffb339a8de54aa99a602ab3f3521
Sergio <111708707+Sergio-Hernandez-Galaz@users.noreply.github.com>
2023-07-04 11:23:45 -0400

    f

--- pages/02_🧬_Transcription Factor.py
+++ pages/02_🧬_Transcription Factor.py
@@ -28,7 +28,7 @@ def upload_sc(adata):
     # IMPORTANT: Cache the conversion to prevent computation on every rerun
     return sc.read_h5ad(adata)
 ##############################################################################################################
-st.dataframe(df,use_container_width=False)
+st.dataframe(df,use_container_width=True)
 st.divider()
 top = st.sidebar.multiselect("Top Regulons",regulons_list)
 encoded_image_list = []
